3efa9793b69dbf81cedb3f016c28ed575eb25abc
Omkar G <ogomkargavhane@gmail.com>
2024-11-15 00:04:46 +0530

    update API url

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,1 @@
-// export const API_URL = "https://ticket-api-production-610a.up.railway.app"
-export const API_URL = "http://localhost:8080"
+export const API_URL = window.location.pathname.includes("localhost") ? "http://localhost:8080" : "https://ticket-api-production-610a.up.railway.app"
